5e386c7eb3f5592c9c846501952fc0495945b93d
AnetaVonkova <137218392+AnetaVonkova@users.noreply.github.com>
2023-09-07 11:06:59 +0200

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,19 +1,19 @@
 -- Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)? -- 
 
 WITH growth AS 
-  (SELECT 
-	  price1.food_category,
-	  price1.AVG_price,
-	  price1.YEAR,
-	  price2.YEAR + 1 AS YEAR_prev,
-	  price2.AVG_price AS price_prev,
-	  ROUND((price1.AVG_price - price2.AVG_price)/price2.AVG_price*100, 2) AS price_growth
-  FROM t_Aneta_Vonkova_project_SQL_primary_final AS price1
-  JOIN t_Aneta_Vonkova_project_SQL_primary_final AS price2
-	ON price1.food_category = price2.food_category
-	  AND price1.YEAR = price2.YEAR + 1
-  GROUP BY food_category,
-		 YEAR )
+	(SELECT 
+		price1.food_category,
+		price1.AVG_price,
+		price1.YEAR,
+		price2.YEAR + 1 AS YEAR_prev,
+		price2.AVG_price AS price_prev,
+		ROUND((price1.AVG_price - price2.AVG_price)/price2.AVG_price*100, 2) AS price_growth
+	FROM t_Aneta_Vonkova_project_SQL_primary_final AS price1
+	JOIN t_Aneta_Vonkova_project_SQL_primary_final AS price2
+		ON price1.food_category = price2.food_category AND 
+		price1.YEAR = price2.YEAR + 1
+  	GROUP BY food_category,
+		YEAR)
 SELECT 
 	food_category, 
 	SUM(price_growth) AS slow_price_increas
