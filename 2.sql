a4eda97247eb8bc424ba7e61ccd15aafc101bfe4
AnetaVonkova <137218392+AnetaVonkova@users.noreply.github.com>
2023-09-08 12:12:36 +0200

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,38 +1,25 @@
 -- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?--
 
 WITH milk_and_bread AS 
-(
-  (SELECT
-	  food_category,
-	  AVG_price,
-  	price_value,
-	  price_unit,
-	  YEAR,
-	  ROUND(AVG(average_wages),2) AS average_wages
-  FROM t_Aneta_Vonkova_project_SQL_primary-final
-  GROUP BY YEAR,
-		      food_category
-  HAVING food_category IN ('Mléko polotučné pasterované','Chléb konzumní kmínový')
-  ORDER BY YEAR ASC
-  LIMIT 2) 
-UNION 
-  (SELECT
-  	food_category,
-	  AVG_price,
-	  price_value,
-	  price_unit,
-	  YEAR,
-	  ROUND(AVG(average_wages),2) AS average_wages
-  FROM t_Aneta_Vonkova_project_SQL_primary-final
-  GROUP BY YEAR,
-		       food_category
-  HAVING food_category IN ('Mléko polotučné pasterované','Chléb konzumní kmínový')
-  ORDER BY YEAR DESC 
-  LIMIT 2)
-)
+	(
+	SELECT
+		food_category,
+		AVG_price,
+		price_value,
+		price_unit,
+		YEAR,
+		average_wages
+	FROM t_Aneta_Vonkova_project_SQL_primary-final
+	WHERE food_category IN ('Mléko polotučné pasterované','Chléb konzumní kmínový') AND 
+		`YEAR` IN (2006, 2018)
+	GROUP BY YEAR,
+		 food_category
+	)
 SELECT 
 	*,
 	ROUND(average_wages/AVG_price, 0) AS available_purchase 
 FROM milk_and_bread ;
+
+
 
 
